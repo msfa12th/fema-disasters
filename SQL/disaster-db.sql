7f15518bed4073bf8215f302d3a24f4141c322e9
Mary Brown <maryadbrown@yahoo.com>
2019-11-06 14:33:42 -0500

    update directory names for consistency

---
+++ SQL/disaster-db.sql
@@ -0,0 +1,12 @@
+-- Database: fema_disasters_db
+
+-- DROP DATABASE fema_disasters_db;
+
+CREATE DATABASE fema_disasters_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
