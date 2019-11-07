cb3c1320ffc1f901df8bbb03e7aef57503872a10
Mary Brown <maryadbrown@yahoo.com>
2019-11-07 00:08:22 -0500

    update insert code for fema, use incident_type instead of event_type

--- SQL/disaster-combine.sql
+++ SQL/disaster-combine.sql
@@ -13,16 +13,10 @@ from tornado;
 commit;
 
 insert into natural_disaster
-select id,data_source,declaration_date,disaster_type,state
+select id,data_source,declaration_date,incident_type,state
 from fema;
 commit;
-delete from earthquake
-where date = '1975-02-23T02:58:41.000Z';
-delete from earthquake
-where date = '2011-03-13T02:23:34.520Z';
-delete from earthquake
-where date = '1985-04-28T02:53:41.530Z';
-commit;
+
 insert into natural_disaster
 (event_id,data_source,event_date,event_type)
 select 
@@ -31,9 +25,18 @@ to_date(date,'mm/dd/yy') ,
 data_source
 from earthquake ;
 
---select count(*) from natural_disaster;
+--found a few bad data elements
+delete from earthquake
+where date = '1975-02-23T02:58:41.000Z';
+delete from earthquake
+where date = '2011-03-13T02:23:34.520Z';
+delete from earthquake
+where date = '1985-04-28T02:53:41.530Z';
+commit;
 
 select * from earthquake limit 10;
+
+
 
 
 
